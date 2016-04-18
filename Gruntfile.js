ee2acfa0b4b2e0c3e1f58fa61ccc18556a123ce2
Gabriel Csapo <gabecsapo@gmail.com>
2016-04-17 22:12:33 -0700

    updates readme with library stats

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var fs = require("fs");
+var filesize = require('file-size');
+
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -7,6 +10,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-autoshot');
     grunt.loadNpmTasks('grunt-contrib-rename');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-readme');
 
     grunt.initConfig({
         stylus: {
@@ -85,6 +89,13 @@ module.exports = function(grunt) {
             }
         },
         clean: ['examples/assets/local-1920x1080-doc.png']
+    });
+
+    grunt.registerTask('size', function() {
+        var output = "";
+        output += "> main-size: " + filesize(fs.statSync("psychic.css")["size"]).human() + '\n';
+        output += "> minified-size: " + filesize(fs.statSync("psychic-min.css")["size"]).human() + '\n';
+        console.log(output);
     });
 
     grunt.registerTask('start', ['default', 'connect', 'autoshot', 'rename', 'clean', 'watch']);
